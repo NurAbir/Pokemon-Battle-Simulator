a58dcb22ee5b6f1d867817ab2532606da8b5c895
Nur Abir <abirhasan.monipur@gmail.com>
2025-12-06 23:00:22 +0600

    Initial commit on Frame

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'login' });
+});
+
+app.get("/signup", (req, res) => {
+    res.render('signup', { title: 'signup' });
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on Port: ${port}`);
+})
