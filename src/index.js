2da2bdb6ddd1282b9d6a96048a1252ac906f3138
Nur Abir <abirhasan.monipur@gmail.com>
2025-12-07 00:16:52 +0600

    Updated index.js with new changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const { error } = require('console');
+const e = require('express');
 
 const app = express();
 
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'views'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'login' });
+    res.render('login', { title: 'login', error: null, email: null });
 });
 
 app.get("/signup", (req, res) => {
