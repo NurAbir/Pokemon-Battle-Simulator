a5821e8f4b8a41afe842069462597f4684a9f806
SafkatRafi22 <rafx.rahmanrafi04@gmail.com>
2025-12-08 13:43:33 +0600

    trying again

--- server/models/User.js
+++ server/models/User.js
@@ -42,34 +42,20 @@ const userSchema = new mongoose.Schema({
   isBanned: {
     type: Boolean,
     default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
 }, { timestamps: true });
 
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+// Hash password before saving - NO NEXT NEEDED
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare passwords
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
-};
-
-// Method to update battle statistics
-userSchema.methods.updateBattleStats = async function() {
-  const Battle = mongoose.model('Battle');
-  const battles = await Battle.find({
-    $or: [{ player1Id: this.userId }, { player2Id: this.userId }]
-  });
-  
-  // Statistics will be calculated from battles
-  return battles;
 };
 
 module.exports = mongoose.model('User', userSchema);
